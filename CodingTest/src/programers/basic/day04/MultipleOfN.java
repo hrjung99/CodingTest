824a0370181c678e10440b83ebf66a213d1d1e85
paramy <rimiajung@gmail.com>
2024-04-17 18:35:22 +0900

    프로그래머스_기초_Day4_공배수

--- CodingTest/src/programers/basic/day04/MultipleOfN.java
+++ CodingTest/src/programers/basic/day04/MultipleOfN.java
@@ -1,8 +1,9 @@
  package programers.basic.day04;
 
 //n의 배수 
+ //정수 num과 n이 매개 변수로 주어질 때, num이 n의 배수이면 1을 return n의 배수가 아니라면 0을 return하도록 solution 함수를 완성해주세요.
 public class MultipleOfN {
-    public int solution(int num, int n) {
+    public static int solution(int num, int n) {
         //n이 num 의 배수 = num/n 했을 때 나머지가 0
         int answer = 0;
         
@@ -14,4 +15,10 @@ public class MultipleOfN {
         
         return answer;
     }
+    
+	public static void main(String[] args) {
+		int answer = solution(10, 2);
+		System.out.println(answer);
+	}
+
 }
